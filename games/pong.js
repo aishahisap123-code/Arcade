bf609c5c502635127ce815b92d0ba9a319692bdc
axisap <aishahisap123@gmail.com>
2026-02-18 16:49:37 +0000

    added overlay system and scoring and smooth animation

--- games/pong.js
+++ games/pong.js
@@ -61,4 +61,109 @@ function resetBall() {
     playerY = e.clientY - rect.top - paddleHeight / 2;
  });
 
- 
+function draw() {
+    ctx.fillSyle = "black";
+    ctx.fillRect(0,0,canvas.width, canvas.height)
+
+    ctx.fillStyle = "#39ff14";
+
+    ctx.fillRect(10, playerY, paddleWidth, paddleHeight);
+
+    ctx.fillRect(canvas.wifth - 22, aiY, paddleWidth, paddleHeight);
+
+    ctx.fillRect(ballX, ballY, ballSize, ballSize);
+
+    for (let i = 0; i < canvas.height; i+= 20) {
+        ctx.fillRect(canvas.width / 2-1, 1, 2, 10);
+    }
+
+    ctx.font = "20px 'Press Start 2P'";
+    ctx.fillText(playerScore, canvas.width / 4, 40);
+    ctx.fillText(aiScore, canvas.width * 3 / 4, 40);
+
+}
+
+function update() {
+    if (!gameRunning) return;
+
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    if (ballY <= 0 || ballY >= canvas.height - ballSize) {
+        ballSpeedY *= -1;
+    }
+
+    if (
+        ballX <= 22 &&
+        ballY > playerY &&
+        ballY < playerY + paddleHeight
+    ) {
+        ballSpeedX *= -1;
+
+        let deltaY = ballY - (playerY + paddleHeight / 2);
+        ballSpeedY = deltaY * 0.15;
+    }
+
+    if (
+        ballX >= canvas.width - 34 &&
+        ballY > aiY &&
+        ballY < aiY + paddleHeight
+    ) {
+        ballSpeedX *= -1;
+
+        let deltaY = ballY - (aiY + paddleHeight / 2);
+        ballSpeedY = deltaY * 0.15;
+    }
+
+    aiY += (ballY - (aiY + paddleHeight / 2)) * 0.08;
+
+    if (ballX > canvas.width) {
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        resetBall;
+    }
+
+    if (ballX < 0) {
+        aiScore++;
+        resetBall();
+    }
+
+    if (playerScore >= winScore) {
+        endGame("YOU WIN!");
+    }
+
+    if (aiScore >= winScore) {
+        endGame("AI WINS!");
+    }
+
+    draw();
+
+}
+
+function gameLoop() {
+    update();
+    requestAnimationFrame(gameLoop);
+}
+
+canvas.addEventListener("click", () => {
+    if (!gameRunning) {
+        startMessage.classList.add("hidden")
+        gameRunning = true;
+    }
+});
+
+function endGame(message) {
+    gameRunning = false;
+
+    winnerText.textContent = message;
+    gameOverOverlay.classList.remove("hidden");
+}
+
+restartBtn.addEventListener("click", () => {
+    gameOverOverlay.classList.add("hidden");
+    startMessage.classList.remove("hidden");
+    initGame();
+});
+
+initGame();
+gameLoop();
